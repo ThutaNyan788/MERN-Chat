616bead3d6ccea4bbfe56d65f22f60d20fe05625
thutanyan <thutathuta319@gmail.com>
2024-05-13 11:48:47 +0630

    who is online

--- api/index.js
+++ api/index.js
@@ -130,4 +130,13 @@ wss.on("connection", (connection,req)=>{
             }
         }
     }
+
+
+    [...wss.clients].forEach((client)=>{
+        client.send(JSON.stringify({
+            online: [...wss.clients].map((c)=> ({userId:c.userId,username:c.username}))
+        }));
+    });
+
 })
+
