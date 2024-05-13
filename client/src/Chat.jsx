23bc7aad11638f12d994c21fb0b601302ffab5b1
thutanyan <thutathuta319@gmail.com>
2024-05-13 13:10:25 +0630

    selecting conversation

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,10 +1,14 @@
-import {useEffect, useState} from "react";
+import {useContext, useEffect, useState} from "react";
 import Avatar from "./Avatar.jsx";
+import Logo from "./Logo.jsx";
+import {UserContext} from "./UserContext.jsx";
 
 export default function Chat()
 {
     const [ws,setWs] = useState(null);
     const [onlinePeople,setOnlinePeople] = useState({});
+    const [selectedUserId,setSelectedUserId] = useState(null);
+    const {username,id} = useContext(UserContext);
 
     useEffect(()=>{
         const ws = new WebSocket("ws://localhost:4040");
@@ -34,26 +38,37 @@ export default function Chat()
         }
     }
 
+    const onlinePeopleIsOurUser = {...onlinePeople};
+
+    delete  onlinePeopleIsOurUser[id];
+
+
     return(
         <div className='flex h-screen'>
-            <div className="bg-white w-1/3 pl-4 pt-4">
-                <div className="text-blue-600 font-bold flex gap-2 mb-4">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-                        <path d="M4.913 2.658c2.075-.27 4.19-.408 6.337-.408 2.147 0 4.262.139 6.337.408 1.922.25 3.291 1.861 3.405 3.727a4.403 4.403 0 0 0-1.032-.211 50.89 50.89 0 0 0-8.42 0c-2.358.196-4.04 2.19-4.04 4.434v4.286a4.47 4.47 0 0 0 2.433 3.984L7.28 21.53A.75.75 0 0 1 6 21v-4.03a48.527 48.527 0 0 1-1.087-.128C2.905 16.58 1.5 14.833 1.5 12.862V6.638c0-1.97 1.405-3.718 3.413-3.979Z" />
-                        <path d="M15.75 7.5c-1.376 0-2.739.057-4.086.169C10.124 7.797 9 9.103 9 10.609v4.285c0 1.507 1.128 2.814 2.67 2.94 1.243.102 2.5.157 3.768.165l2.782 2.781a.75.75 0 0 0 1.28-.53v-2.39l.33-.026c1.542-.125 2.67-1.433 2.67-2.94v-4.286c0-1.505-1.125-2.811-2.664-2.94A49.392 49.392 0 0 0 15.75 7.5Z" />
-                    </svg>
-
-                    Mern Chat</div>
-                {Object.keys(onlinePeople).map(userId=>(
-                    <div key={userId} className='border-b border-gray-100 py-2
-                    flex items-center gap-2'>
-                        <Avatar username={onlinePeople[userId]} userId={userId}/>
-                        <span className={"text-gray-800"}>{onlinePeople[userId]}</span>
+            <div className="bg-white w-1/3">
+                <Logo/>
+                {Object.keys(onlinePeopleIsOurUser).map(userId=>(
+                    <div onClick={()=>setSelectedUserId(userId)} key={userId} className={`border-b border-gray-100 py-2
+                        flex items-center gap-2 cursor-pointer  ${selectedUserId == userId ? "bg-blue-100" : ""}`}>
+                        {userId === selectedUserId && (
+                            <div className={"w-1 bg-blue-500 h-12 rounded-r-md"}></div>
+                        )}
+                        <div className={"flex gap-2 py-2 pl-4 items-center"}>
+                            <Avatar username={onlinePeople[userId]} userId={userId}/>
+                            <span className={"text-gray-800"}>{onlinePeople[userId]}</span>
+                        </div>
                     </div>
                 ))}
             </div>
-            <div className="flex flex-col bg-blue-300 w-2/3 p-2">
-                <div className='flex-grow'>message with selected person</div>
+            <div className="flex flex-col bg-blue-50 w-2/3 p-2">
+                <div className='flex-grow'>
+                    {!selectedUserId && (
+                        <div className={"flex h-full flex-grow items-center justify-center"}>
+                                <div className={"text-lg text-gray-500"}>&larr; Select a person</div>
+                        </div>
+                    )}
+                </div>
+
                 <div className='flex gap-2'>
                     <input type="text" placeholder="Type your message here"
                     className="bg-white border p-2 flex-grow
